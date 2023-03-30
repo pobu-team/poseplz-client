e067f3b7905d0632617cfc1438d342cf7f580cae
Minhye Kang <alsgp9898@naver.com>
2023-03-31 00:13:07 +0900

    💄 Global Style, Theme 속성 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,6 +10,14 @@ const GlobalStyle = createGlobalStyle`
   *::after {
     box-sizing: inherit;
   }
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
   html {
     font-size: 62.5%;
   }
@@ -17,8 +25,13 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     height: 100%;
     font-size: 1.6rem;
+    font-family: 'Pretendard-Regular';
+    color: ${props => props.theme.colors.text};
     background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text}
+  }
+
+  p, button, span {
+    font-family: 'Pretendard-Regular';
   }
 
   :lang(ko) {
--- src/styles/defaultTheme.ts
+++ src/styles/defaultTheme.ts
@@ -2,14 +2,25 @@ const defaultTheme = {
 	colors: {
 		background: '#FFF',
 		text: '#000',
+		reverseText: '#FFF',
 		layoutBackground: '#F1F3F5',
-		primary: '#CED4DA',
+		primary: '#BBFF65',
+		startButton: '#BBFF65',
+		border: '#BBFF65',
+		buttonBackground: '#F2F2F7',
+		detailButton: '#FFFFFF',
+		randomButton: '#000000',
+		arrowBackground: '#FFFFFF',
 	},
 
 	sizes: {
 		layoutWidth: '430px',
 		contentPadding: '1rem',
 		contentBorderRadius: '8rem',
+	},
+
+	img: {
+		check: 'images/check.png',
 	},
 };
 
