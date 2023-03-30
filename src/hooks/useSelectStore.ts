4575b2bd695e457e277f74218f3455e2e26b0f0a
Minhye Kang <alsgp9898@naver.com>
2023-03-31 00:31:32 +0900

    ✏️ 함수 이름 수정

--- src/hooks/useSelectStore.ts
+++ src/hooks/useSelectStore.ts
@@ -4,7 +4,7 @@ import SelectStore, {type SelectStoreSnapshot} from '../stores/SelectStore';
 
 const selectStore = new SelectStore();
 
-export default function useCounterStore(): [SelectStoreSnapshot, SelectStore] {
+export default function useSelectStore(): [SelectStoreSnapshot, SelectStore] {
 	const snapshot = useSyncExternalStore(
 		onStoreChange => {
 			selectStore.addListener(onStoreChange);
