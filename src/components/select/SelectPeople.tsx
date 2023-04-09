5ea15fc191eca77f12cf1865c75162425d96df79
Minhye Kang <alsgp9898@naver.com>
2023-04-09 19:31:50 +0900

    ♻️ Refactor: 인원 선택 페이지 disabled 삭제

--- src/components/select/SelectPeople.tsx
+++ src/components/select/SelectPeople.tsx
@@ -8,69 +8,83 @@ import useSelectStore from '../../hooks/useSelectStore';
 
 type PersonButtonProps = {
 	active: boolean;
+	index: number;
 };
 
 const Container = styled.div`
-  padding: 10px;
+  padding: ${props => props.theme.sizes.contentPadding};
   display: flex;
   flex-direction: column;
-  align-items: center;
 
   h1 {
-    font-size: 25px;
-    font-weight: 600;
-    margin-bottom: 15px;
+    font-size: 1.8em;
+    font-weight: bold;
+    letter-spacing: -0.67px;
+    line-height: 1.36;
+    margin-bottom: 32px;
   }
   
   div {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-column-gap: 10px;
     width: 100%;
   }
+
+  @media screen and (max-width: 300px){
+      font-size: 13px;
+    }
+  
 `;
 
 const PersonButton = styled.button<PersonButtonProps>`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    margin: 8px;
-    padding: 20px;
-    width: 156px;
-    height: 180px;
+    padding: 30px 25px 15px;
+    margin-bottom: 16px;
+    width: 100%;
     border-radius: 16px;
     border: none;
+    font-size: 1.1em;
+    font-weight: 600;
     color: ${props => props.theme.colors.text};
     background: ${props => props.theme.colors.buttonBackground};
     cursor: pointer;
 
+    
     ${props => props.active && css`
     border: 3px solid ${props.theme.colors.primary};
     background-color: ${props.theme.colors.background};
-  `};
+    `};
+    
+    img {
+      width: 100%;
+      margin-bottom: 15px;
+    };
 
-  &:disabled {
-    opacity: 0.5;
-  }
+    ${props => props.index === 1 && css`
+      padding: 25px 40px 15px;
+      img {
+        margin-bottom: 25px;
+      }
+    `};
 
-  img {
-    width: 100%;
-    padding: 10px;
-    height: auto;
-  }
+    ${props => props.index >= 5 && css`
+      img{
+        margin-bottom: 5px;
+      }  
+    `};
 `;
 
 const NextButton = styled.button`
   margin-top: 10px;
   display: flex;
-  width: 350px;
-  height: 50px;
+  width: 100%;
+  height: 2.6em;
   justify-content: center;
   align-items: center;
   border: none;
   border-radius: 10px;
-  font-size: 20px;
+  font-size: 1.3em;
   font-weight: 600;
   cursor: pointer;
   color: '#000';
@@ -84,33 +98,14 @@ const NextButton = styled.button`
 export default function SelectPeople() {
 	const navigate = useNavigate();
 
-	const [, store] = useSelectStore();
+	const [,store] = useSelectStore();
 
-	const initialState: Record<number, boolean> = {};
-	for (let i = 1; i <= 6; i++) {
-		initialState[i] = false;
-	}
+	console.log(store.personNum);
 
-	const [active, setActive] = useState(initialState);
-
-	const [isBtnDisabled, setIsBtnDisabled] = useState([false, false, false, false, false, false]);
-
-	const [isNextBtnDisabled, setIsNextBtnDisabled] = useState(true);
+	const [_, setIsNextBtnDisabled] = useState(true);
 
 	const handleClickPersonNum = (number: number) => {
 		store.savePersonNum(String(number));
-
-		if (active[number]) {
-			setActive(Object.assign(active, {[number]: false}));
-			setIsBtnDisabled(new Array(6).fill(false));
-			setIsNextBtnDisabled(true);
-			return;
-		}
-
-		let newBtnState = [...isBtnDisabled];
-		newBtnState = newBtnState.map((_, index) => (index !== number - 1));
-		setIsBtnDisabled(newBtnState);
-		setActive(Object.assign(active, {[number]: true}));
 		setIsNextBtnDisabled(false);
 	};
 
@@ -120,12 +115,13 @@ export default function SelectPeople() {
 			<div>
 				{[1, 2, 3, 4, 5, 6].map((item, index) => (
 					<PersonButton
-						type='button' key={item}
+						type='button'
+						key={item}
+						index={item}
 						onClick={() => {
 							handleClickPersonNum(item);
 						}}
-						active={active[item]}
-						disabled={isBtnDisabled[index]}
+						active={store.personNum === String(item)}
 					>
 						<img src={`/images/person-${index + 1}.png`} alt='person'/>
 						{item}명
@@ -136,7 +132,7 @@ export default function SelectPeople() {
 				onClick={() => {
 					navigate('/theme');
 				}}
-				disabled={isNextBtnDisabled}
+				disabled={store.personNum === ''}
 			>다음</NextButton>
 		</Container>
 	);
