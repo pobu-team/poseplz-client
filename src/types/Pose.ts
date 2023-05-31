4b48ee8777cc6ddcdf194e41808ea08cc66dc186
Minhye Kang <alsgp9898@naver.com>
2023-05-31 18:12:28 +0900

    🔥 remove mock data

--- src/types/Pose.ts
+++
@@ -1,9 +0,0 @@
-import type Image from './Image';
-
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-interface Pose {
-	id: string;
-	image: Image[];
-}
-
-export default Pose;
