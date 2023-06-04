5246db3fd9724acfbb3165f9d2492d2e6c288b8b
Minhye Kang <alsgp9898@naver.com>
2023-06-04 22:25:52 +0900

    ✨ Feat: 테마 선택 페이지 변경

---
+++ src/components/select/SelectTheme.styles.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-column-gap: 16px;
+  width: 100%;
+  margin-bottom: 30px;
+`
+
+export const Container = styled.div`
+  padding: ${(props) => props.theme.sizes.contentPadding};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  h1 {
+		margin: 0 10px;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 20px;
+  }
+
+  h2 {
+		margin-left: 20px;
+    font-size: 20px;
+    font-weight: 600;
+  }
+`;
+
+export const SubmitBtnContainer = styled.div`
+  display: flex;
+  width: 100%;
+
+  button {
+    border-radius: 10px;
+    height: 58px;
+    padding: 0px;
+    border: none;
+    font-size: 20px;
+    font-weight: 600;
+  }
+  
+
+  button:first-child{
+    color: ${(props) => props.theme.colors.background};
+    background-color: ${(props) => props.theme.colors.randomButton};
+    flex: 2;
+    margin-right: 5px;
+  }
+  
+  button:last-child{
+    cursor: pointer;
+    flex: 1.5;
+    margin-left: 5px;
+    color: '#000';
+    background-color: ${(props) => props.theme.colors.border};
+  }
+
+  button:disabled {
+		opacity: 0.5;
+  }
+`;
--- src/components/select/SelectTheme.tsx
+++ src/components/select/SelectTheme.tsx
@@ -1,106 +1,73 @@
-import {useEffect, useState} from 'react';
-import {useNavigate, useParams} from 'react-router';
-import styled from 'styled-components';
-import useSelectStore from '../../hooks/useSelectStore';
-import SpecialDay from './SpecialDay';
-import Theme from './Theme';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
 
-const Container = styled.div`
-  padding: ${(props) => props.theme.sizes.contentPadding};
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+import { ONE_PERSON, THREE_MORE_PERSON, TWO_PERSON, themeObj } from '../../constant/tag';
 
-  h1 {
-		margin: 0 10px;
-    font-size: 28px;
-    font-weight: 600;
-    margin-bottom: 20px;
-  }
+import SquareButton from '../../ui/SquareButton';
 
-  h2 {
-		margin-left: 20px;
-    font-size: 20px;
-    font-weight: 600;
-  }
-`;
+import useFetchTag from '../../hooks/useFetchTag';
 
-const SubmitBtnContainer = styled.div`
-  display: flex;
-  width: 100%;
-
-  button:first-child{
-    color: ${props => props.theme.colors.background};
-    background-color: ${props => props.theme.colors.randomButton};
-    flex: 1
-  }
-  
-  button {
-    cursor: pointer;
-    border-radius: 20px;
-    flex: 2;
-    margin: 10px;
-    height: 58px;
-    border: none;
-    font-size: 20px;
-    font-weight: 600;
-    color: '#000';
-    background-color: ${props => props.theme.colors.border};
-  }
-
-  button:disabled {
-		opacity: 0.5;
-  }
-`;
+import { ButtonContainer, Container, SubmitBtnContainer } from './SelectTheme.styles';
 
 export default function SelectTheme() {
-	const navigate = useNavigate();
-	const {id = ''} = useParams();
+  const navigate = useNavigate();
+  
+  const [isDisable, setIsDisable] = useState(true);
+  const [isRandomDisable, setIsRandomDisable] = useState(false);
+  
+  const { id='' } = useParams();
+  
+  let themeArr: string[] = [];
+  if(id === '1') themeArr = ONE_PERSON;
+  if(id === '2') themeArr = TWO_PERSON;
+  if(id !== '1' && id !== '2') themeArr = THREE_MORE_PERSON;
+  
+  const [clickedTheme, setClickedTheme] = useState('');
+  
+  const [personNumTag,_] = useFetchTag(id, clickedTheme);
+  // const themeIds = theme.map((tag: Tag) => tag.tagId);
+  const handleClickTheme = (theme: string) => {
+    setClickedTheme(String(theme));
+    setIsDisable(false);
+  }
 
-	const [{theme}] = useSelectStore();
+  const handleClickRandom = () => {
+    navigate(`/pose/${id}/random`);
+  };
 
-	const [isDisable, setIsDisable] = useState(true);
-	const [isRandomDisable, setIsRandomDisable] = useState(false);
-
-	useEffect(() => {
-		if (theme.length !== 0) {
-			setIsDisable(false);
-			setIsRandomDisable(true);
-		}
-
-		if (theme.length === 0) {
-			setIsDisable(true);
-			setIsRandomDisable(false);
-		}
-	});
-
-	const themeUrl = theme.join('&');
-
-	const handleClickRandom = () => {
-		navigate(`/pose/${id}/random`);
-	};
-
-	return (
-		<Container>
-			<h1>원하시는 테마를 선택해주세요</h1>
-			<h2>특별한 날 이신가요?</h2>
-			<SpecialDay />
-			<h2>이런 분위기로 찍고싶어!</h2>
-			<Theme />
-			<SubmitBtnContainer>
-				<button
-					type='button'
-					onClick={handleClickRandom}
-					disabled={isRandomDisable}
-				>랜덤</button>
-				<button
-					type='button'
-					onClick={() => {
-						navigate(`/pose/${id}/${themeUrl}`);
-					}}
-					disabled={isDisable}
-				>다음</button>
-			</SubmitBtnContainer>
-		</Container>
-	);
+  return (
+    <Container>
+      <h1>원하시는 테마를 선택해주세요</h1>
+      <ButtonContainer>
+        {themeArr.map((theme: string, index: number) => (
+          <SquareButton
+            key={theme+index}
+            text={theme}
+            imgSrc={`/images/theme-${themeObj[theme]}.svg`}
+            active={clickedTheme === theme}
+            onClickFunc={() => handleClickTheme(theme)}
+          />
+          ))}
+      </ButtonContainer>
+      <div style={{height: '100px'}}></div>
+      <SubmitBtnContainer>
+        <button
+          type="button"
+          onClick={handleClickRandom}
+          disabled={isRandomDisable}
+        >
+          그냥 넘어갈래요
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(`/pose/${id}/${personNumTag[0].tagId}`);
+          }}
+          disabled={isDisable}
+        >
+          결과보기
+        </button>
+      </SubmitBtnContainer>
+    </Container>
+  );
 }
