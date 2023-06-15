d1185059a68177d58d44de5ddd549572857fd7ab
Minhye Kang <alsgp9898@naver.com>
2023-06-15 14:57:16 +0900

    💄 Style: '그냥 넘어갈래요' 버튼에 cursor : pointer 추가

--- src/components/select/SelectTheme.styles.tsx
+++ src/components/select/SelectTheme.styles.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ButtonContainer = styled.div`
   display: grid;
@@ -7,7 +7,7 @@ export const ButtonContainer = styled.div`
   grid-column-gap: 16px;
   width: 100%;
   margin-bottom: 30px;
-`
+`;
 
 export const Container = styled.div`
   padding: ${(props) => props.theme.sizes.contentPadding};
@@ -16,14 +16,14 @@ export const Container = styled.div`
   align-items: flex-start;
 
   h1 {
-		margin: 0 10px;
+    margin: 0 10px;
     font-size: 28px;
     font-weight: 600;
     margin-bottom: 20px;
   }
 
   h2 {
-		margin-left: 20px;
+    margin-left: 20px;
     font-size: 20px;
     font-weight: 600;
   }
@@ -41,24 +41,24 @@ export const SubmitBtnContainer = styled.div`
     font-size: 20px;
     font-weight: 600;
   }
-  
 
-  button:first-child{
+  button:first-child {
+    cursor: pointer;
     color: ${(props) => props.theme.colors.background};
     background-color: ${(props) => props.theme.colors.randomButton};
     flex: 2;
     margin-right: 5px;
   }
-  
-  button:last-child{
+
+  button:last-child {
     cursor: pointer;
     flex: 1.5;
     margin-left: 5px;
-    color: '#000';
+    color: "#000";
     background-color: ${(props) => props.theme.colors.border};
   }
 
   button:disabled {
-		opacity: 0.5;
+    opacity: 0.5;
   }
 `;
