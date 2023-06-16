46ff89dbdc4b2ce3819548e16a12d5cecdfd4df6
Minhye Kang <alsgp9898@naver.com>
2023-06-16 18:05:23 +0900

    ✨ Feat: 랜덤인 경우 태그 6개로 확장

--- src/constant/tag.ts
+++ src/constant/tag.ts
@@ -1,14 +1,15 @@
 export const themeObj: themeObj = {
-  '분위기있게': '1',
-  '예쁘게': '2',
-  '재미있게': '3',
-  '귀엽게': '4',
-  '연인사이': '5',
-  '친구끼리': '6',
-}
+  분위기있게: '1',
+  예쁘게: '2',
+  재미있게: '3',
+  귀엽게: '4',
+  연인사이: '5',
+  친구끼리: '6',
+};
 
 type themeObj = Record<string, string>
 
 export const ONE_PERSON = ['분위기있게', '예쁘게', '재미있게', '귀엽게'];
 export const TWO_PERSON = ['연인사이', '친구끼리', '재미있게', '귀엽게'];
 export const THREE_MORE_PERSON = ['친구끼리', '예쁘게', '재미있게', '귀엽게'];
+export const RANDOM = ['분위기있게', '예쁘게', '재미있게', '귀엽게', '연인사이', '친구끼리'];
