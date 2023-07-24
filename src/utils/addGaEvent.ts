a2c52d606d74c8f4eaf71be88a17d8bfd2b34e0a
ckhe1215 <ckhe1215@gmail.com>
2023-07-24 23:12:56 +0900

    📦 Chore: dev 도메인 GA 차단

--- src/utils/addGaEvent.ts
+++ src/utils/addGaEvent.ts
@@ -1,7 +1,8 @@
 import ReactGA from 'react-ga4';
 
 const addGaEvent = (eventName: string) => {
-  if (!window.location.href.includes('localhost')) {
+  if (!window.location.href.includes('localhost')
+    && (!window.location.href.includes('pobu-pose-recommend.vercel.app'))) {
     ReactGA.event({
       category: 'Event',
       action: eventName,
