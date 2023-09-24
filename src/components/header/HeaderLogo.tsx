9a3354e47a0276ec442cfc135a6694f600c9d5d8
himyne <alsgp9898@naver.com>
2023-09-24 20:37:58 +0900

    💄 style: 메인페이지를 제외하면 헤더 로고가 가운데에 있도록하는 조건 추가

--- src/components/header/HeaderLogo.tsx
+++ src/components/header/HeaderLogo.tsx
@@ -1,12 +1,16 @@
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { useReadLocalStorage } from 'usehooks-ts';
 
-const Container = styled.div`
+const Container = styled.div<{isNotRouteMain: boolean}>`
   display: flex;
   justify-content: center;
+  
+  ${(props) => props.isNotRouteMain && css`
+    flex: 1;
+  `}
 `;
 
 const Logo = styled.button`
@@ -31,13 +35,15 @@ export default function HeaderLogo() {
   const navigate = useNavigate();
 
   const isDarkMode = useReadLocalStorage('darkMode');
+  const location = useLocation();
+  const isNotRouteMain = location.pathname !== '/main';
 
   const handleClickLogo = () => {
     navigate('/main');
   };
 
   return (
-    <Container>
+    <Container isNotRouteMain={isNotRouteMain}>
       <Logo onClick={handleClickLogo}>
         <img
           src={isDarkMode ? '/images/logo_D.svg' : '/images/logo.svg'}
