85c6034c70e0a6e3a73ac4c6135f9baa8e247dd8
강민혜 <alsgp9898@naver.com>
2023-11-05 16:50:54 +0900

    feat: 리액트 쿼리 최상단에 QueryProvider 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import { RecoilRoot } from 'recoil';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
 import App from './App';
 import Loading from './components/common/Loading';
 
 window.Kakao.init(process.env.REACT_APP_KAKAO_JS_KEY ?? '');
+const queryClient = new QueryClient();
 
 function main() {
   const container = document.getElementById('root');
@@ -20,7 +23,10 @@ function main() {
   root.render(
     <RecoilRoot>
       <React.Suspense fallback={<Loading />}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </React.Suspense>
     </RecoilRoot>,
   );
