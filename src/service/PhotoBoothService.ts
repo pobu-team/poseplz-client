ddf200beaba3179c4998019d9b8754fe6f5934d8
강민혜 <alsgp9898@naver.com>
2023-12-03 20:45:54 +0900

    feat: 포토부스 apiService 생성, 포토부스 응답 타입 생성, 쿼리 작성

---
+++ src/service/PhotoBoothService.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { PhotoBoothResponse } from '../types/PhotoBooth';
+
+const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/api/v1` || 'https://server.poseplz.com/api/v1';
+
+class PhotoBoothService {
+  private readonly instance = axios.create({
+    baseURL: API_BASE_URL,
+  });
+
+  async fetchPhotoBooths() {
+    const { data } = await this.instance.get<PhotoBoothResponse>('/photo-booths');
+    return data;
+  }
+}
+
+const photoBoothService = new PhotoBoothService();
+export default photoBoothService;
